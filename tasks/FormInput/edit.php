482ac535ae7d2fd31d96dcfc28ca186562ae8491
mraeb <abhishekbalajisaravanan@gmail.com>
2026-02-13 13:38:26 +0530

    input formats added, the user should enter the correct values to store into db

--- tasks/FormInput/edit.php
+++ tasks/FormInput/edit.php
@@ -29,11 +29,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <table>
             <tr>
                 <td><label for="uname">Name:</label></td>
-                <td><input type="text" name="uname" value="<?php echo $row['uname']; ?>" required></td>
+                <td><input type="text" name="uname" pattern="[a-zA-Z .]+" value="<?php echo $row['uname']; ?>" required></td>
             </tr>
             <tr>
                 <td><label for="age">Age:</label></td>
-                <td><input type="number" name="age" value="<?php echo $row['age']; ?>" required></td>
+                <td><input type="text" name="age" pattern ="\d{2}" value="<?php echo $row['age']; ?>" required></td>
             </tr>
             <tr>
                 <td><label for="gender">Gender:</label></td>
@@ -44,11 +44,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </tr>
             <tr>
                 <td><label for="mobile">Mobile:</label></td>
-                <td><input type="tel" name="mobile" value="<?php echo $row['mobile']; ?>" required></td>
+                <td><input type="tel" name="mobile" pattern="[6-9][0-9]{9}" value="<?php echo $row['mobile']; ?>" required></td>
             </tr>
             <tr>
                 <td><label for="email">Email:</label></td>
-                <td><input type="email" name="email" value="<?php echo $row['email']; ?>" required></td>
+                <td><input type="email" name="email" pattern="[a-z0-9.]+@[a-z]{5,}.[a-z]{2,}" value="<?php echo $row['email']; ?>" required></td>
             </tr>
         </table>
         <button type="submit">Update</button>
--- tasks/FormInput/index.php
+++ tasks/FormInput/index.php
@@ -13,11 +13,11 @@
             <table>
                 <tr>
                     <td><label for="uname">Name: </label></td>
-                    <td><input id="uname" type="text" name="uname" placeholder="Enter Your Name..." required></td>
+                    <td><input id="uname" type="text" name="uname" pattern="[a-zA-Z .]+" placeholder="Enter Your Name..." required></td>
                 </tr>
                 <tr>
                     <td><label for="age">Age: </label></td>
-                    <td><input type ="number" id="age" name="age" placeholder="Enter your age..." required></td>
+                    <td><input type ="text" id="age" name="age" pattern ="\d{2}" placeholder="Enter your age..." required></td>
                 </tr>
                 <tr>
                     <td><label for="gender">Gender: </label></td>
@@ -29,11 +29,11 @@
                     </td>
                 <tr>
                     <td><label for="mobile">Mobile No: </label></td>
-                    <td><input type="tel" id="mobile" name="mobile"  placeholder="Enter your Mobile..." required></td>
+                    <td><input type="tel" id="mobile" name="mobile" pattern="[6-9][0-9]{9}" placeholder="Enter your Mobile..." required></td>
                 </tr>
                 <tr>
                     <td><label for="email">Email ID: </label></td>
-                    <td><input type="email" id="email" name="email"  placeholder="Enter your Email..." required></td>
+                    <td><input type="email" id="email" name="email" pattern="[a-z0-9.]+@[a-z]{5,}.[a-z]{2,}" placeholder="Enter your Email..." required></td>
                 </tr>
                 
                 
