7e4e8528f3303141bd5f33b4428abdbe275088ba
mraeb <mraebs@gmail.com>
2026-04-08 07:18:04 +0530

    A PHP program to find given number is even or odd

---
+++ numberInputCheck/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check Number Input</title>
+</head>
+<body>
+    <form method="post" action="index.php">
+        <label for="number">Enter a number:</label>
+        <input type="text" id="number" name="number">
+        <input type="submit" value="Submit">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+        $number = $_POST['number'];
+        if (is_numeric($number) && $number%2==0) {
+            echo "<p>The input is a even number: $number</p>";
+        } else {
+            echo "<p>The input is a odd number.</p>";
+        }
+    }
+    ?>
+</body>
+</html>
